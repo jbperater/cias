1f8c0bf7fb229eb340709bbad6b86797902d3a9f
Christian H. Cat-awan <christian.cat-awan@ustp.edu.ph>
2019-03-04 16:23:35 +0800

    1

--- application/views/addEventRequest.php
+++ application/views/addEventRequest.php
@@ -125,14 +125,55 @@
                                 </div>   
                             </div>
                         </div><!-- /.box-body -->
-    
                         <div class="box-footer">
-                            <input type="submit" class="btn btn-primary" data-toggle="modal" data-target="#myModal"value="Submit" />
+                            <input type="submit" class="btn btn-primary" value="Submit" />
                             <input type="reset" class="btn btn-default" value="Reset" />
                         </div>
                     </form>
                 </div>
             </div>
+            <div class="col-md-4">
+                <?php
+                    $this->load->helper('form');
+                    $error = $this->session->flashdata('error');
+                    if($error)
+                    {
+                ?>
+                <div class="alert alert-danger alert-dismissable">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                    <?php echo $this->session->flashdata('error'); ?>                    
+                </div>
+                <?php } ?>
+                <?php  
+                    $success = $this->session->flashdata('success');
+                    if($success)
+                    {
+                ?>
+                <div class="alert alert-success alert-dismissable">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                    <?php echo $this->session->flashdata('success'); ?>
+                </div>
+                <?php } ?>
+                
+                <div class="row">
+                    <div class="col-md-12">
+                        <?php echo validation_errors('<div class="alert alert-danger alert-dismissable">', ' <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button></div>'); ?>
+                    </div>
+                </div>
+            </div>
+        </div>    
+    </section>
+    
+</div>
+<script src="<?php echo base_url(); ?>assets/js/addUser.js" type="text/javascript"></script>
+    
+                       <!--  <div class="box-footer">
+                            <input type="submit" class="btn btn-primary" data-toggle="modal" data-target="#myModal"value="Submit" />
+                            <input type="reset" class="btn btn-default" value="Reset" />
+                        </div>
+                    </form>
+                </div>
+            </div> -->
             <!-- <div class="modal fade" id="myModal" role="dialog">
             <div class="modal-dialog"> -->
             
