997a6958183c099211a5352041fe7510cc614a80
Christian H. Cat-awan <christian.cat-awan@ustp.edu.ph>
2019-03-04 15:26:36 +0800

    Update addEventRequest.php

--- application/views/addEventRequest.php
+++ application/views/addEventRequest.php
@@ -133,11 +133,11 @@
                     </form>
                 </div>
             </div>
-            <div class="modal fade" id="myModal" role="dialog">
-            <div class="modal-dialog">
+            <!-- <div class="modal fade" id="myModal" role="dialog">
+            <div class="modal-dialog"> -->
             
               <!-- Modal content-->
-              <div class="modal-content">
+             <!--  <div class="modal-content">
                 <div class="modal-header">
                   <button type="button" class="close" data-dismiss="modal">&times;</button>
                   <h4 class="modal-title">Notice</h4>
@@ -176,4 +176,4 @@
             });
     <?php } ?>
 
-</script>
+</script> -->
